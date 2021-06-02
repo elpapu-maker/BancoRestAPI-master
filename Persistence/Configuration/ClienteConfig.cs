3b8ff7a332ca8001b9c0028de3af4dbcc1349359
William <lopezvasquezwillianalexander@gmail.com>
2021-06-02 00:07:59 -0600

    Agregar archivos de proyecto.

---
+++ Persistence/Configuration/ClienteConfig.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configuration
+{
+    public class ClienteConfig : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> Builder)
+        {
+            Builder.ToTable("Clientes");
+
+            Builder.HasKey(p => p.id);
+
+            Builder.Property(p => p.Nombre)
+                   .HasMaxLength(80)
+                   .IsRequired();
+
+            Builder.Property(p => p.Apellido)
+                   .HasMaxLength(80)
+                   .IsRequired();
+
+            Builder.Property(p => p.Telefono)
+                   .HasMaxLength(9)
+                   .IsRequired();
+
+            Builder.Property(p => p.Email)
+                   .HasMaxLength(100);
+
+            Builder.Property(p => p.Direccion)
+                   .HasMaxLength(120)
+                   .IsRequired();
+
+            Builder.Property(p => p.Edad);
+
+            Builder.Property(p => p.CreatedBy)
+                   .HasMaxLength(30);
+
+            Builder.Property(p => p.LastModifiedBy)
+                   .HasMaxLength(30);
+
+        }
+    }
+}
---
+++ Persistence/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Persistence.Repository;
+
+namespace Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void AddPersistenceInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            #region Repositories
+            services.AddTransient(typeof(IRepositoryAsync<>), typeof(MyRepositoryAsync<>));
+            #endregion
+        }
+    }
+}
